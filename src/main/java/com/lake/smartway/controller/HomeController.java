f956e6ea7af19ba5830fc36495be78209e0f0150
lake <lake1310@gmail.com>
2020-05-12 23:25:24 +0900

    add comments

--- src/main/java/com/lake/smartway/controller/HomeController.java
+++ src/main/java/com/lake/smartway/controller/HomeController.java
@@ -3,6 +3,8 @@ package com.lake.smartway.controller;
 import com.lake.smartway.service.SecurityService;
 import com.lake.smartway.service.SecurityServiceImpl;
 import com.lake.smartway.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +17,14 @@ import java.util.Map;
 @RestController
 @RequestMapping("/")
 public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private SecurityService securityService;
 
     @GetMapping("security/generate/token")
     public Map<String, Object> generateToken(@RequestParam String subject){
+        logger.debug("JWT Generate Token");
         String token = securityService.createToken(subject, 1000 * 60 * 60);
         HashMap<String, Object> map = new HashMap<>();
         map.put("userId", subject);
@@ -31,6 +35,7 @@ public class HomeController {
 
     @GetMapping("security/get/subject")
     public String getSubject(@RequestParam String token){
+        logger.debug("Check Invalid JWT Token");
         String subject = securityService.getSubejct(token);
         return subject;
     }
