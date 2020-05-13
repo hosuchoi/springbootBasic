6a218b9b7df07f84e7b4d72d76d0c2fa63ab67f7
최호수 <hosu.choi@miracom.co.kr>
2020-05-13 17:43:35 +0900

    application listener

--- src/main/java/com/lake/smartway/applicationListener/StartedEventListener.java
+++ src/main/java/com/lake/smartway/applicationListener/StartedEventListener.java
@@ -5,12 +5,11 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SampleListener implements ApplicationListener<ApplicationStartedEvent> {  //ApplicationStartedEvent 는 Application Context가 만들어지기 전에 이벤트 발생
-
+public class StartedEventListener implements ApplicationListener<ApplicationStartedEvent> {
     @Override
     public void onApplicationEvent(ApplicationStartedEvent applicationStartedEvent) {
         System.out.println("=============================");
-        System.out.println("Application is starting");
+        System.out.println("Application is started");
         System.out.println("=============================");
     }
 }
---
+++ src/main/java/com/lake/smartway/applicationListener/StartingEventListener.java
@@ -0,0 +1,19 @@
+package com.lake.smartway.applicationListener;
+
+import com.lake.smartway.SmartwayApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+//@Component  // SmartwayApplication에서 직접 등록 했기 때문에 빈등록 의미가 없음
+public class StartingEventListener implements ApplicationListener<ApplicationStartingEvent> {  //ApplicationStartingEvent 는 Application Context가 만들어지기 전에 이벤트 발생
+
+    @Override
+    public void onApplicationEvent(ApplicationStartingEvent applicationStartingEvent) {
+        System.out.println("=============================");
+        System.out.println("Application is starting");
+        System.out.println("=============================");
+    }
+}
