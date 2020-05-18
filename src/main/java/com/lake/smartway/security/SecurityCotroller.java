8918ad8db6eae672f1508cb8f2e37703d201a364
lake <lake1310@gmail.com>
2020-05-19 03:55:26 +0900

    secu add

--- src/main/java/com/lake/smartway/security/SecurityCotroller.java
+++ src/main/java/com/lake/smartway/security/SecurityCotroller.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
     5) spring security 의존성 추가
     6) 기본 static 패키지에 index 페이지 생성 후  http://localhost:8080 호출 -> spring에서 제공해주는 login page 열림
     7) 사용자ID : user    password : 웹서버 구동시마다 새로 생김 (Using generated security password: f6fdf8b0-fd9e-40f6-8fe7-a1050823bf47)
+    8) entity,service,repository 생성
+    9) SecurityConfig -> WebSecurityConfigurerAdapter 상속받는 Bean등록해서 인증할 요청과 안할 요청 정의
+    10) AccountService -> UserDetailsService 상속받는 bean을 등록해서 실제 User 생성및 인증 로직 추가
+    11) spring security에서 passwordEncoder 생성
  */
 @Controller
 public class SecurityCotroller {
